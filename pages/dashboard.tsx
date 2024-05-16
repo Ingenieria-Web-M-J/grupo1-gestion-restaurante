349412c2df543911bd10c79a654ae578323c90bc
yepesDev <dyepes@syg.com.co>
2024-05-15 19:21:40 -0500

    front: ejemplo de crud

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,14 +1,17 @@
-import { SidePanel } from "@/components/Transactions/modules";
+import { BodyDashboard, SidePanel } from "@/components/Transactions/modules";
 
 export default function Dashboard() {
     return (
-        <div className="grid lg:grid-cols-12 md:grid-cols-1 sm:grid-cols-1 grid-cols-1 gap-0 h-full">
-            <div className="lg:col-span-2 md:grid-cols-1 sm:grid-cols-1 grid-cols-1 bg-white">
-                <SidePanel></SidePanel>
-            </div>
-            <div className="lg:col-span-10 md:grid-cols-1 sm:grid-cols-1 grid-cols-1 bg-gray-200">
-
-            </div>
+        <div>
+                <div className="grid lg:grid-cols-12 md:grid-cols-1 sm:grid-cols-1 grid-cols-1 gap-0 h-full">
+                    <div className="lg:col-span-2 md:grid-cols-1 sm:grid-cols-1 grid-cols-1 bg-white">
+                        <SidePanel></SidePanel>
+                    </div>
+                    <div className="lg:col-span-10 md:grid-cols-1 sm:grid-cols-1 grid-cols-1 bg-gray-200">
+                        <BodyDashboard></BodyDashboard>
+                    </div>
+                </div>
+            <script src="../path/to/flowbite/dist/flowbite.min.js"></script>
         </div>
 
     );
